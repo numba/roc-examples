3bf06aefc0e1ade75d0664cdeef04d20b31cbe11
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-10-06 17:32:33 -0500

    Work on multivariate kde

--- numba_hsa_examples/kerneldensity/cpu_ref.py
+++ numba_hsa_examples/kerneldensity/cpu_ref.py
@@ -9,6 +9,8 @@ from bokeh.plotting import figure, show, output_file, vplot
 
 from numba import njit
 
+np.random.seed(12345)
+
 
 @njit
 def gaussian(x, mu, sigma):
@@ -31,18 +33,44 @@ def approx_gaussian_bandwidth(xs):
 
 def uni_kde_seq_factory(kernel):
     @njit
-    def kde(support, samples, bandwidth, pdf):
+    def uni_kde(support, samples, bandwidth, pdf):
         for i in range(support.size):
-            xi = support[i]
+            supp = support[i]
             total = 0
             for j in range(samples.size):
-                total += kernel((xi - samples[j]) / bandwidth) / bandwidth
+                total += kernel((samples[j] - supp) / bandwidth) / bandwidth
             pdf[i] = total / samples.size
 
-    return kde
+    return uni_kde
 
 
 uni_kde_seq = uni_kde_seq_factory(gaussian_kernel)
+
+
+def multi_kde_seq_factory(kernel):
+    @njit
+    def multi_kde(support, samples, bandwidths, pdf):
+        """
+        Expects 2d arrays for samples and support: (num_observations,
+        num_variables)
+        """
+        assert support.shape[1] == samples.shape[1]
+        nvar = support.shape[1]
+        for i in range(support.shape[0]):
+            sum = 0
+            for j in range(samples.shape[0]):
+                prod = 1
+                for k in range(nvar):
+                    bw = bandwidths[k]
+                    diff = samples[j, k] - support[i, k]
+                    prod *= kernel(diff / bw) / bw
+                sum += prod
+            pdf[i] = sum / samples.shape[0]
+
+    return multi_kde
+
+
+multi_kde_seq = multi_kde_seq_factory(gaussian_kernel)
 
 
 def test_gaussian():
@@ -58,28 +86,27 @@ def test_gaussian():
     show(p1)
 
 
-def test():
-    np.random.seed(12345)
+def build_support(samples, bandwidth, cut=3):
+    minimum = np.min(samples) - cut * bandwidth
+    maximum = np.max(samples) + cut * bandwidth
+    nobs = samples.size
+    support = np.linspace(minimum, maximum, nobs)
+    return support
 
+
+def test_uni():
     samples = mixture_rvs([.25, .75], size=10000,
                           dist=[stats.norm, stats.norm],
                           kwargs=(
                               dict(loc=-1, scale=.5), dict(loc=1, scale=.5)))
 
     # samples = np.random.normal(size=10000)
-
-
-
     bandwidth = approx_gaussian_bandwidth(samples)
-    bandwidth /= 2
+
     print('bandwidth', bandwidth)
     print('size', samples.size)
 
-    cut = 3
-    minimum = np.min(samples) - cut * bandwidth
-    maximum = np.max(samples) + cut * bandwidth
-    nobs = samples.size
-    support = np.linspace(minimum, maximum, nobs)
+    support = build_support(samples, bandwidth)
     pdf = np.zeros_like(support)
 
     uni_kde_seq(support, samples, bandwidth, pdf)
@@ -108,8 +135,51 @@ def test():
     show(vplot(p1, p2, p3))
 
 
-#
+def test_multi():
+    samples = np.ascontiguousarray(np.vstack([np.random.normal(size=1000),
+                                              np.random.normal(size=1000)]).T)
+
+    bwlist = [approx_gaussian_bandwidth(samples[:, k])
+              for k in range(samples.shape[1])]
+    bandwidths = np.array(bwlist)
+
+    support = np.ascontiguousarray(
+        np.vstack([build_support(samples[:, i], bandwidths[i])
+                   for i in range(bandwidths.size)]).T)
+
+    pdf = np.zeros(support.shape[0], dtype=np.float64)
+
+    print('bandwidths', bandwidths)
+    print('support', support.shape)
+    print('samples', samples.shape)
+
+    multi_kde_seq(support, samples, bandwidths, pdf)
+
+    # print(pdf)
+
+    # Plotting
+    output_file("kde_multi.html")
+    #
+    # p1 = figure(title="Hist")
+    # hist, edges = np.histogram(samples, bins=50, density=True)
+    # p1.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:])
+
+    p2 = figure(title="KDE")
+    p2.line(support[:, 0], pdf, color='blue')
+    p2.line(support[:, 1], pdf, color='red')
+    # p2.circle(x=support, y=pdf, size=5)
+
+    p3 = figure(title="KDE-SM")
+    kde = sm.nonparametric.KDEMultivariate(samples, var_type='cc')
+    kde_pdf = kde.pdf()
+
+    idx = support[:, 0].argsort(kind='mergesort')
+    p3.line(support[idx, 0], kde_pdf[idx], color='blue')
+
+    show(vplot(p2, p3))
+
 
 if __name__ == '__main__':
-    test()
+    # test_uni()
     # test_gaussian()
+    test_multi()
