259d2d5b0808cf3647b63dea9abae6a58166268a
Siu Kwan Lam <michael.lam.sk@gmail.com>
2015-10-12 16:38:13 -0500

    Add proper testing for KDE

--- numba_hsa_examples/kerneldensity/cpu_ref.py
+++ numba_hsa_examples/kerneldensity/cpu_ref.py
@@ -1,4 +1,5 @@
 import math
+import itertools
 import numpy as np
 from scipy import stats
 
@@ -6,6 +7,7 @@ import statsmodels.api as sm
 from statsmodels.distributions.mixture_rvs import mixture_rvs
 
 from bokeh.plotting import figure, show, output_file, vplot
+from bokeh import palettes
 
 from numba import njit
 
@@ -26,9 +28,13 @@ def gaussian_kernel(x):
     return gaussian(x, mu=0, sigma=1)
 
 
-def approx_gaussian_bandwidth(xs):
-    stddev = np.std(xs)
-    return ((4 * stddev ** 5) / (3 * xs.size)) ** (1 / 5)
+def approx_bandwidth(xs):
+    """
+    Scott's rule of thumb as in SciPy
+    """
+    n = xs.size
+    d = xs.ndim
+    return n ** (-1 / (d + 4))
 
 
 def uni_kde_seq_factory(kernel):
@@ -94,6 +100,21 @@ def build_support(samples, bandwidth, cut=3):
     return support
 
 
+def calc_rms(expect, got, norm=False):
+    """
+    Caclualte the RMS error between two arrays of the same size.
+
+    If `norm` is True, the result is normalized to the range of the expected
+    distribution.
+    """
+    assert expect.size == got.size
+    rms = np.sqrt(np.sum((expect - got) ** 2) / expect.size)
+    if norm:
+        return rms / np.ptp(expect)
+    else:
+        return rms
+
+
 def test_uni():
     samples = mixture_rvs([.25, .75], size=10000,
                           dist=[stats.norm, stats.norm],
@@ -101,7 +122,7 @@ def test_uni():
                               dict(loc=-1, scale=.5), dict(loc=1, scale=.5)))
 
     # samples = np.random.normal(size=10000)
-    bandwidth = approx_gaussian_bandwidth(samples)
+    bandwidth = approx_bandwidth(samples)
 
     print('bandwidth', bandwidth)
     print('size', samples.size)
@@ -135,17 +156,20 @@ def test_uni():
     show(vplot(p1, p2, p3))
 
 
-def test_multi():
-    samples = np.ascontiguousarray(np.vstack([np.random.normal(size=1000),
-                                              np.random.normal(size=1000)]).T)
+def test_multi_kde():
+    nelem = 100
 
-    bwlist = [approx_gaussian_bandwidth(samples[:, k])
+    samples = np.ascontiguousarray(np.vstack([np.random.normal(size=nelem),
+                                              np.random.normal(size=nelem)]).T)
+
+    bwlist = [approx_bandwidth(samples[:, k])
               for k in range(samples.shape[1])]
     bandwidths = np.array(bwlist)
 
-    support = np.ascontiguousarray(
-        np.vstack([build_support(samples[:, i], bandwidths[i])
-                   for i in range(bandwidths.size)]).T)
+    support_dims = [build_support(samples[:, i], bandwidths[i])
+                    for i in range(bandwidths.size)]
+
+    support = np.array([(x, y) for x, y in itertools.product(*support_dims)])
 
     pdf = np.zeros(support.shape[0], dtype=np.float64)
 
@@ -155,31 +179,133 @@ def test_multi():
 
     multi_kde_seq(support, samples, bandwidths, pdf)
 
-    # print(pdf)
+    kde = sm.nonparametric.KDEMultivariate(samples, var_type='cc',
+                                           bw=bwlist)
+    expect_pdf = kde.pdf(support)
 
     # Plotting
+
     output_file("kde_multi.html")
-    #
-    # p1 = figure(title="Hist")
-    # hist, edges = np.histogram(samples, bins=50, density=True)
-    # p1.quad(top=hist, bottom=0, left=edges[:-1], right=edges[1:])
 
-    p2 = figure(title="KDE")
-    p2.line(support[:, 0], pdf, color='blue')
-    p2.line(support[:, 1], pdf, color='red')
-    # p2.circle(x=support, y=pdf, size=5)
+    cm = RGBAColorMapper(0, 1, palettes.Spectral11)
 
-    p3 = figure(title="KDE-SM")
-    kde = sm.nonparametric.KDEMultivariate(samples, var_type='cc')
-    kde_pdf = kde.pdf()
+    p1 = figure(title="KDE")
+    p1.square(support[:, 0], support[:, 1], size=5,
+              color=cm.color(pdf / np.ptp(pdf)))
 
-    idx = support[:, 0].argsort(kind='mergesort')
-    p3.line(support[idx, 0], kde_pdf[idx], color='blue')
+    p2 = figure(title="KDE-SM")
+    p2.square(support[:, 0], support[:, 1], size=5,
+              color=cm.color(expect_pdf / np.ptp(expect_pdf)))
 
-    show(vplot(p2, p3))
+    show(vplot(p1, p2))
+
+
+def test_uni_kde():
+    samples = mixture_rvs([.25, .75], size=10000,
+                          dist=[stats.norm, stats.norm],
+                          kwargs=(
+                              dict(loc=-1, scale=.5), dict(loc=1, scale=.5)))
+
+    # samples = np.random.normal(size=10000)
+    bandwidth = approx_bandwidth(samples)
+
+    # Run statsmodel for reference
+    kde = sm.nonparametric.KDEUnivariate(samples)
+    kde.fit(kernel="gau")
+
+    # Reuse statsmodel support for our test
+    support = kde.support
+
+    # Run custom KDE
+    pdf = np.zeros_like(support)
+    uni_kde_seq(support, samples, bandwidth, pdf)
+
+    # Check value
+    expect = kde.density
+    got = pdf
+
+    rms = calc_rms(expect, got, norm=True)
+    assert rms < 1e-4, "RMS error too high"
+
+
+# Color map
+def hex_to_rgb(value):
+    """Given a color in hex format, return it in RGB."""
+
+    values = value.lstrip('#')
+    lv = len(values)
+    rgb = list(int(values[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+    return rgb
+
+
+class RGBAColorMapper(object):
+    """Maps floating point values to rgb values over a palette"""
+
+    def __init__(self, low, high, palette):
+        self.range = np.linspace(low, high, len(palette))
+        # self.r, self.g, self.b = np.array(zip(*[hex_to_rgb(i) for i in palette])) #python 2.7
+        self.r, self.g, self.b = np.array(
+            list(zip(*[hex_to_rgb(i) for i in palette])))
+
+    def color(self, data):
+        """Maps your data values to the pallette with linear interpolation"""
+
+        red = np.interp(data, self.range, self.r)
+        blue = np.interp(data, self.range, self.b)
+        green = np.interp(data, self.range, self.g)
+        # Style plot to return a grey color when value is 'nan'
+        red[np.isnan(red)] = 240
+        blue[np.isnan(blue)] = 240
+        green[np.isnan(green)] = 240
+        colors = np.dstack([red.astype(np.uint8),
+                            green.astype(np.uint8),
+                            blue.astype(np.uint8),
+                            np.full_like(data, 255, dtype=np.uint8)])
+        colors = colors.view(dtype=np.uint32).reshape(data.shape)
+
+        def make_hex(x):
+            h = hex(x)[4:]
+            diff = 6 - len(h)
+            prefix = '0' * diff
+            return prefix + h
+
+        return ["#{0}".format(make_hex(x)) for x in colors]
+
+
+def test_multi_kde():
+    nelem = 100
+
+    samples = np.ascontiguousarray(np.vstack([np.random.normal(size=nelem),
+                                              np.random.normal(size=nelem)]).T)
+
+    bwlist = [approx_bandwidth(samples[:, k])
+              for k in range(samples.shape[1])]
+    bandwidths = np.array(bwlist)
+
+    support_dims = [build_support(samples[:, i], bandwidths[i])
+                    for i in range(bandwidths.size)]
+
+    support = np.array([(x, y) for x, y in itertools.product(*support_dims)])
+
+    pdf = np.zeros(support.shape[0], dtype=np.float64)
+
+    print('bandwidths', bandwidths)
+    print('support', support.shape)
+    print('samples', samples.shape)
+
+    multi_kde_seq(support, samples, bandwidths, pdf)
+
+    kde = sm.nonparametric.KDEMultivariate(samples, var_type='cc',
+                                           bw=bwlist)
+    expect_pdf = kde.pdf(support)
+
+    rms = calc_rms(expect_pdf, pdf, norm=True)
+    assert rms < 1e-4, "RMS error too high"
 
 
 if __name__ == '__main__':
+    test_multi_kde()
+    # test_uni_kde()
     # test_uni()
     # test_gaussian()
-    test_multi()
+    # test_multi()
